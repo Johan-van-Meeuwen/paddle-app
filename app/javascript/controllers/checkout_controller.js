bd15603e67a13551f9f821b2851b120627345a39
Johan van Meeuwen <vanmeeuwen.j@gmail.com>
2022-11-13 17:55:37 +0000

    Added plain success page

--- app/javascript/controllers/checkout_controller.js
+++ app/javascript/controllers/checkout_controller.js
@@ -1,12 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["monthlyCheckout", "annualCheckout"]
+  static targets = ["monthlyCheckout", "annualCheckout", "checkoutId"]
+
+  // checkoutComplete() {
+  //   alert("Thanks!")
+  // }
 
   openCheckout() {
     Paddle.Checkout.open({
-      product: event.path[0].attributes[1].nodeValue,
-      success: 'success'
+      product: event.target.getAttribute('data-product'),
+      // successCallback: checkoutComplete,
+      success: '/success'
     });
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  get "success", to: "pages#success"
 end
